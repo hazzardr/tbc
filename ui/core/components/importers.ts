584e8cf38b5c855e44a79f5df3bdcf94b4cb9228
Kayla Glick <12898988+kayla-glick@users.noreply.github.com>
2026-02-24 18:21:40 -0500

    Update importers.ts

--- ui/core/components/importers.ts
+++ ui/core/components/importers.ts
@@ -278,6 +278,7 @@ class IndividualAddonImporter<SpecType extends Spec> extends Importer {
 
 		const gearJson = importJson['gear'];
 		gearJson.items = (gearJson.items as Array<any>).filter(item => item != null);
+		delete gearJson.version;
 		const equipmentSpec = EquipmentSpec.fromJson(gearJson);
 
 		this.finishIndividualImport(this.simUI, charClass, race, equipmentSpec, talentsStr);
